c1c1e4ece6651a15956687e87a1f8d924f6ef9f2
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2019-12-18 02:46:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var http = require('http')
 
 var port = 4000
 
+var os = require("os");
+
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World build test!\n')
+  response.end('Hello World build test! \n' + os.hostname())
 })
 
 server.listen(port)
